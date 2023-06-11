38cbee6c0afd22a2d65e1657d4f0affb47da2cbe
Trent Weiss <ttw2xk@virginia.edu>
2023-06-11 13:52:29 -0400

    compile EKF and UKF into a composable node library

---
+++ src/composable_nodes.cpp
@@ -0,0 +1,5 @@
+
+#include "robot_localization/ros_filter_types.hpp"
+#include <rclcpp_components/register_node_macro.hpp>  // NOLINT
+RCLCPP_COMPONENTS_REGISTER_NODE(robot_localization::RosEkf)
+RCLCPP_COMPONENTS_REGISTER_NODE(robot_localization::RosUkf)
